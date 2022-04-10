511d40d8a0582e832a2052fc71439bd079a94376
MaddieLymei <97503724+MaddieLymei@users.noreply.github.com>
2022-04-10 19:52:19 +0200

    Update printBudgetsForUser_Procedure.sql

--- printBudgetsForUser_Procedure.sql
+++ printBudgetsForUser_Procedure.sql
@@ -3,7 +3,7 @@ GO
 
 CREATE PROCEDURE SelectUserBudget @UserID int
 AS
-SELECT b.[description], b.startDate, b.EndDate, ie.[Description], cft.[Description], cf.CashFlowName, cf.CashFlowAmount
+SELECT b.BudgetName, b.startDate, b.EndDate, ie.[Description], cft.[Description], cf.CashFlowName, cf.CashFlowAmount
 FROM Budget b
 INNER JOIN CashFlowBudget cfb
 ON b.BudgetID = cfb.BudgetID
@@ -14,5 +14,3 @@ ON cf.CashFlowTypeID = cft.CashFlowTypeID
 INNER JOIN IncomeAndExpense ie
 ON ie.IncomeAndExpenseID = cft.IncomeAndExpenseID
 WHERE b.UserID = @UserID
-
-EXEC SelectUserBudget @UserID = 1
