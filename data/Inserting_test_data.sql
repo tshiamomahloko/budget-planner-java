67e467c9cb7072635f88568eb911fa6490d583ea
Paulo NGOVE <paulon@bbd.co.za>
2022-04-19 18:07:00 +0200

    test data for dev db

---
+++ data/Inserting_test_data.sql
@@ -0,0 +1,70 @@
+  /*INSERTING TEST DATA INTO THE BUDGET TABLE*/
+  insert into budget
+  Values(1, '12/8/2021', 'Savings', '1/01/2022')
+  
+  insert into budget
+  Values(2, '12/8/2021', 'My Budget', '1/01/2022')
+  
+  insert into budget
+  Values(3, '12/8/2021', 'May 2022', '1/01/2022')
+  
+  insert into budget
+  Values(4, '12/8/2021', 'Festival', '1/01/2022')
+
+  insert into budget
+  Values(5, '12/8/2021', 'Vacation', '1/01/2022')
+
+  /*INSERTING TEST DATA INTO THE USER TABLE*/
+  insert into app_users
+  Values(1, 'user1@email.com', 'User 1', 'User@1223')
+
+    insert into app_users
+  Values(2, 'Joe@email.com', 'Joe Doe', 'JoeDoe1')
+
+    insert into app_users
+  Values(3, 'Dave@email.com', 'Dave Mayer', 'Dave123')
+
+    insert into app_users
+  Values(4, 'Make@email.com', 'Mike Mutton', 'MikeM')
+
+    insert into app_users
+  Values(5, 'Qwerty@email.com', 'A Qwerty', 'Qwerty')
+
+  /*INSERTING TEST DATA INTO THE CASHFLOW TABLE*/
+  insert into cash_flow
+  values(1, 'Salary')
+insert into cash_flow
+  values(2, 'Forex')
+  insert into cash_flow
+  values(3, 'Savings')
+  insert into cash_flow
+  values(4, 'Trading')
+  insert into cash_flow
+  values(5, 'Business')
+
+
+  /*INSERTING TEST DATA INTO THE BUDGET_CASHFLOW TABLE*/
+  insert into budget_cash_flow
+  Values(1, 3)
+    insert into budget_cash_flow
+  Values(1, 5)
+    insert into budget_cash_flow
+  Values(2, 1)
+    insert into budget_cash_flow
+  Values(2, 2)
+    insert into budget_cash_flow
+  Values(3, 1)
+    insert into budget_cash_flow
+  Values(4, 5)
+    insert into budget_cash_flow
+  Values(4, 3)
+    insert into budget_cash_flow
+  Values(4, 2)
+    insert into budget_cash_flow
+  Values(4, 1)
+    insert into budget_cash_flow
+  Values(4, 4)
+    insert into budget_cash_flow
+  Values(5, 1)
+    insert into budget_cash_flow
+  Values(5, 2)
