8b399eb71d3fb0ce1160c02297114981a1b3c639
Madeleine Lambley <madeleinel@bbd.co.za>
2022-04-20 11:59:34 +0200

    Budget Update

--- budgetapp/src/main/java/com/javalevelup/budgetapp/BudgetappApplication.java
+++ budgetapp/src/main/java/com/javalevelup/budgetapp/BudgetappApplication.java
@@ -1,6 +1,9 @@
 package com.javalevelup.budgetapp;
 
 import com.javalevelup.budgetapp.Budget.BudgetRepository;
+import com.javalevelup.budgetapp.Budget.BudgetService;
+import com.javalevelup.budgetapp.Customer.Customer;
+import com.javalevelup.budgetapp.Customer.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.github.javafaker.Faker;
@@ -11,6 +14,7 @@ import com.javalevelup.budgetapp.Budget.Budget;
 import com.javalevelup.budgetapp.CashFlow.CashFlow;
 
 import java.sql.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class BudgetappApplication {
@@ -21,15 +25,17 @@ public class BudgetappApplication {
 
 	}
 	@Bean
-	CommandLineRunner commandLineRunner(BudgetRepository br) {
+	CommandLineRunner commandLineRunner(BudgetRepository br, BudgetService bs, UserRepository cr) {
 		return args -> {
 			Faker faker = new Faker();
+			Customer customer = new Customer("Tori");
+			cr.save(customer);
 
-			Budget budget = new Budget("April", new Date(1649769300000L), new Date(1651324500000L));
-			Budget budget2 = new Budget("May", new Date(1651324500000L), new Date(1653916500000L));
+			Budget budget = new Budget("April", new Date(1649769300000L), new Date(1651324500000L), customer);
+			Budget budget2 = new Budget("May", new Date(1651324500000L), new Date(1653916500000L), customer);
 
-			CashFlow cashFlow = new CashFlow();
-			CashFlow cashFlow2 = new CashFlow();
+			CashFlow cashFlow = new CashFlow("St Lucia", 1021020.0 );
+			CashFlow cashFlow2 = new CashFlow("Cats cats cats", -1212151.50);
 			budget.addCashFlowToBudget(cashFlow);
 			budget2.addCashFlowToBudget(cashFlow2);
 
@@ -37,9 +43,17 @@ public class BudgetappApplication {
 			br.save(budget2);
 
 			budget2.addCashFlowToBudget(cashFlow);
-
 			br.save(budget2);
 
+			List<Budget> b = bs.getUserBudgets(1L);
+
+			System.out.println("TIME FOR SOME CONSOLE LOGS BITCHES!");
+			b.forEach(BHDJHS -> System.out.println(bs.displayBudget(BHDJHS)));
+			bs.replicateBudget(1L);
+			bs.modifyBudget(3L,"Woof", new Date(1651324500000L), new Date(1651324500000L));
+
+			System.out.println("TIME FOR SOME CONSOLE LOGS BITCHES!");
+			bs.getUserBudgets(1L).forEach(BHDJHS -> System.out.println(bs.displayBudget(BHDJHS)));
 		};
 	}
 }
